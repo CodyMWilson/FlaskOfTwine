387d631de29dce8506a1af35d4a9b511046e6b26
Cody Wilson <codythewilson@gmail.com>
2022-01-22 19:23:35 -0800

    Brainstorming notes

--- newsheet.py
+++ newsheet.py
@@ -60,6 +60,33 @@ def main():
                table = soup.find('script')#, attrs = {'id':'script'})
                print(table)
                
+
+               #now that we have the data, we have some processing to do... 
+               # 1 - the html option "tags" is ALL part of the alias box
+               # 2 - anything in a 'tags' with .fuz or .wav extension is ignored
+               # 3 - the html "name" is the card title and goes into the context box
+               # 4 - body of html is the line of dialogue
+               # 5 - each distinct character should have their own sheet generated (so each can have their own structure)
+               # 6 - Each sheet needs to be created based on a template
+               # 7 - Each sheet needs a quest name 
+               # 8 - Each scene has a delineater between them, and scenes are cards that branch to four other cards
+               # # Note that a branch means that a text option in the body of the option delineated by [[ .. ]] will be the name for another html tag (card
+               # 9 - Cards with a single link can be put at the bottom in close to their original order.
+               # 10 - Player choice sections need to be ordered
+               # 11 - Character lines that flow un-interrupted are designated by having the same character have multiple linear branches and can all be boxed together
+               # 12 - Lines are color coded and can be assigned during runtime
+
+               # This means that as we process we need to keep track of the following for each of x characters:
+               # 1 Individual lines w/
+               # # Character speaking
+               # # Branches
+               # # Context
+               # # 
+               # 2 How many branches each line has
+               # 3  
+
+               # Implementation - 
+               #  Each html (voice line/card) can be an object with number of links, line data, what the links are, color of the table, 
                for row in soup.findAll('tw-passagedata'):
                    print(row.text.strip())
                #html = html_file.read()
