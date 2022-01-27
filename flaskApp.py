e9ab9db5e7b5379ff0017a381aed040b3bf1d74b
Cody Wilson <codythewilson@gmail.com>
2022-01-26 21:42:32 -0800

    Add upload button, fix requirements.

--- flaskApp.py
+++ flaskApp.py
@@ -1,10 +1,22 @@
-print ('hello from python on ubuntu!')
+import os
+from flask import Flask, render_template, request, redirect, url_for
 
-from flask import Flask, render_template
+UPLOAD_FOLDER = os.getcwd()
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+print("hello from python!")
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
-    return ("Hello World!")
+    return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        print("saved!")
+        uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename))
+    return redirect(url_for('index'))
