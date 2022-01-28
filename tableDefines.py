52eb0753c19eb7fef1d392084699eb087019f77b
Cody Wilson <cody.wilson@3dsystems.com>
2022-01-27 16:44:27 -0800

    Add defines

---
+++ tableDefines.py
@@ -0,0 +1,9 @@
+templateText = {'B1' : 'Notes', 'C1' : 'Your Line', 'D1' : 'Context/ABXY', 'E1' : 'Alias'}
+
+columnTemplate = {'Notes' : 'B', 'Your Line' : 'C', 'Context/ABXY' : 'D', 'Alias' : 'E1'}
+
+notes = { 1 : 'Please follow the Recording Guidelines: https://docs.google.com/document/d/1Hwp_cCeh60xjnyq1Fj9wppRSd9jAtrOQ11VlunnMHn8/edit?usp=sharing', 
+
+          2 : 'Recording should be RAW (no EQ/Compression/Noise Reduction etc.)',
+
+          3 : '2 - 3 takes for all lines'}
